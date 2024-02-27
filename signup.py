109c3ee2b5b986318b4cac18847f23ff19f19654
Elisa Skowronek <elisa.skowronek@etu.minesparis.psl.eu>
2024-02-27 16:33:25 +0100

    moche mais marche

--- signup.py
+++ signup.py
@@ -4,14 +4,9 @@ from flask_wtf import FlaskForm, Form
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flask_sqlalchemy import SQLAlchemy
 
-#initialisation des databases
-db = SQLAlchemy()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SECRET KEY']='secret'
-db.init_app(app)
 
 #variables globales (temporaire)
 FIRST_NAME = 'ahhhhh'
@@ -19,15 +14,7 @@ LAST_NAME = 'lol'
 
 d={'first_name':FIRST_NAME, 'last_name':LAST_NAME}
 #inscription
-@app.route('/')
-def index():
-    return render_template('index_resan.html')
-
-@app.route('/home')
-def home():
-    return (str(d['first_name']))
-
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def signup():
     form=MyForm(request.form)
     if form.submit.data:
@@ -36,26 +23,13 @@ def signup():
         return redirect(url_for('home'))
     return render_template('signup.html', form=form)
 
+@app.route('/home')
+def home():
+    return (d['first_name'] + ' ' + d['last_name'])
+
 class MyForm(Form):
     first_name = StringField('First Name')
     last_name  = StringField('Last Name')
-    submit = SubmitField('Submit') ###pb de submit 
-
-
-#un user
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
-
-
-#créer un user
-def create_user(username, email, password):
-    user = User(username=username, email=email, password=password)
-    db.session.add(user)
-    db.session.commit()
+    submit = SubmitField('Submit') 
 
 app.run()

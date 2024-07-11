0c639e5ac8bcaf012f27f5187297e497bb76a664
Colin Drouineau <colin.drouineau@etu.minesparis.psl.eu>
2024-07-11 15:27:47 +0200

    final mail.py

--- mail.py
+++ mail.py
@@ -2,17 +2,16 @@
 # Nécessite de télécharger et de se connecter sur la version Outlook classique
 # (nom de la version à la date de juillet 2024)
 
-import win32com.client
+# from ... import base de données as bd (ici, on prend la base de données d'exemple instance\site.db)
 
-# from ... import base de données as bd
-import sqlite3
-import pandas as pd
-
-import sqlite3
-import pandas as pd
-
+# Ici, le programme fonctionne s'il est exécuté directement.
+# Mais on pourrait le convertir faicilement en fonction si on voulait l'appeler depuis un autre fichier.
 
 if __name__ == "__main__":
+
+    import win32com.client  # Pour l'envoi automatique du mail
+    import sqlite3  # Pour convertir de sql en panda database
+    import pandas as pd  # pour la base de données et la conversion de la base en SQL
 
     # Connect to the SQLite database
     conn = sqlite3.connect("instance\site.db")  # Replace with your database file
@@ -39,10 +38,10 @@ if __name__ == "__main__":
 
     newmail.Subject = "données pour resan"
 
-    newmail.To = "cyrianne.chabert@etu.minesparis.psl.eu"  # recipient string : separate the different recipient by a ';'
+    newmail.To = "recipient_address"  # recipient string : separate the different recipient by a ';'
 
     newmail.HTMLBody = "texte éventuel"
-    attach = r"C:\Users\colin\cours-info\meuh\data.xlsx"
+    attach = r"C:\Users\colin\cours-info\meuh\data.xlsx"  # path\data.xlsx
     newmail.Attachments.Add(attach)
 
     newmail.Send()

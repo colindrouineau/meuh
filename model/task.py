4b962b0393be6a239fde88892551d941cf15cf0a
Colin Drouineau <colin.drouineau@etu.minesparis.psl.eu>
2024-07-18 23:27:40 +0200

    end of project v1

--- model/task.py
+++ model/task.py
@@ -3,9 +3,10 @@ from extension import db
 
 class Task(db.Model):
     """
-        Class Task inherits db.Model
+    Class Task inherits db.Model
 
     """
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     date = db.Column(db.String(100), nullable=False)
